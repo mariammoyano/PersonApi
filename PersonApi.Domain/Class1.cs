9bb146fcee2f2c91e8fa88703c59e219778ee348
Maria Mercedes Moyano <moyanomariamercedes@gmail.com>
2020-11-09 21:20:26 -0300

    Initial commit

---
+++ PersonApi.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PersonApi.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ PersonApi.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PersonApi.Service
+{
+    public class Class1
+    {
+    }
+}
